23a8a18d800c34f1908ed5277c4fc17fcaffaaad
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2016-06-08 15:21:48 +0500

    Add files via upload (part 2)

---
+++ ThreeLittlePigsTests/PigTest.m
@@ -0,0 +1,34 @@
+//
+//  PigTest.m
+//  ThreeLittlePigs
+//
+//  Created by Alexander Gomzyakov on 23.02.14.
+//  Copyright (c) 2014 Alexander Gomzyakov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PigTest : XCTestCase
+
+@end
+
+@implementation PigTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
